00df6304b992e35c425fa676fa308823053b06d5
raffii3424235 <79677667+raffii3424235@users.noreply.github.com>
2021-03-02 07:36:43 +0700

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -5,29 +5,29 @@ module.exports = {
     info: {
         name: "leave",
         aliases: ["goaway", "disconnect"],
-        description: "Leave The Voice Channel!",
+        description: "Keluar Dari Voice Channel!",
         usage: "Leave",
     },
 
     run: async function (client, message, args) {
         let channel = message.member.voice.channel;
-        if (!channel) return sendError("I'm sorry but you need to be in a voice channel!", message.channel);
-        if (!message.guild.me.voice.channel) return sendError("I Am Not In Any Voice Channel!", message.channel);
+        if (!channel) return sendError("Maaf,Anda harus berada di dalam voice channel!", message.channel);
+        if (!message.guild.me.voice.channel) return sendError("Saya Tidak Dalam Voice Channel Manapun!", message.channel);
 
         try {
             await message.guild.me.voice.channel.leave();
         } catch (error) {
             await message.guild.me.voice.kick(message.guild.me.id);
-            return sendError("Trying To Leave The Voice Channel...", message.channel);
+            return sendError("Mencoba Meninggalkan Voice Channel...", message.channel);
         }
 
         const Embed = new MessageEmbed()
-            .setAuthor("Leave Voice Channel", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+            .setAuthor("Meninggalkan Voice Channel", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
             .setColor("GREEN")
             .setTitle("Success")
-            .setDescription("🎶 Left The Voice Channel.")
+            .setDescription("🎶 Meninggalkan Voice Channel.")
             .setTimestamp();
 
-        return message.channel.send(Embed).catch(() => message.channel.send("🎶 Left The Voice Channel :C"));
+        return message.channel.send(Embed).catch(() => message.channel.send("🎶 Meninggalkan Voice Channel :C"));
     },
 };
